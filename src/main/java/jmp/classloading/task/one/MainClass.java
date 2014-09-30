f2dd01579991b0a85d554d07eba03b2f8883fd17
Mikalai Lohach <mikalai_lohach@epam.com>
2014-09-30 12:03:54 +0300

    Initial commit

---
+++ src/main/java/jmp/classloading/task/one/MainClass.java
@@ -0,0 +1,12 @@
+package jmp.classloading.task.one;
+
+/**
+ * @author Mikalai_Lohach
+ *
+ */
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
