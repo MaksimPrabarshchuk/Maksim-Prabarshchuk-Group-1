94adef0f26d76b71f77aa6f9dabc01e9dea94e80
Mikalai Lohach <logachn@gmail.com>
2014-11-02 22:53:03 +0300

    jUnit

--- src/test/java/by/fly/service/UserRepositoryTest.java
+++ src/test/java/by/fly/service/UserRepositoryTest.java
@@ -26,6 +26,7 @@ public class UserRepositoryTest extends AbstractBaseTest {
         userRepository.save(user);
     }
 
+    @Test
     public void findByBarcodeTest() {
         UserRepository userRepositoryMock = Mockito.mock(UserRepository.class);
         when(userRepositoryMock.findByBarcode("abc")).thenReturn(null);
