d0bd4e0674a56afdad26c79451f737fac216695f
Mikalai Lohach <mikalai_lohach@epam.com>
2015-01-06 08:44:55 -0300

    Spring MVC

--- spring-mvc/src/main/java/jmp/spring/mvc/controller/IndexController.java
+++ spring-mvc/src/main/java/jmp/spring/mvc/controller/IndexController.java
@@ -3,6 +3,8 @@ package jmp.spring.mvc.controller;
 import jmp.spring.mvc.service.EmployeeService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +16,8 @@ public class IndexController {
 	private EmployeeService employeeService;
 
     @RequestMapping("/")
-    public String index(Model model) {
-        model.addAttribute("employees", employeeService.findAll());
+    public String index(@PageableDefault(size = 5, page = 0) Pageable page, Model model) {
+        model.addAttribute("employees", employeeService.findAll(page));
         return "index";
     }
 }
